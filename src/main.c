af19c7c9c3323067145a04969058b721e74e2ac9
Duck-Lsouza <lv99134@gmail.com>
2026-01-28 00:58:26 -0300

    feat: implementa menu inicial, adiciona delays e corrige fluxo de telas

--- src/main.c
+++ src/main.c
@@ -2,17 +2,66 @@
 #include <stdlib.h>
 #include "portabilidade.h"
 
+
+
+void menu_ini();
+
 void menu_edit();
 void edit(int numero);
 void erro_opcao();
 
 
+
 int main (){
-
+    menu_ini();
     limpar_tela();
-    menu_edit();
+    
+    
 
 
+}
+
+void menu_ini(){
+    
+    
+    int opcao = 0;
+    
+    while(opcao != 4){
+        limpar_tela();
+        printf("==============================================\n");
+        printf("\n\tMENU PRINCIPAL (INIT-TASK)\n\n");
+        printf("==============================================\n");
+        printf("(1) Criar uma tarefa.\n");
+        printf("(2) Editar tarefas existentes\n");
+        printf("(3) Listar todas as tarefas\n");
+        printf("(4) Exit\n");
+            scanf("%d", &opcao);  
+        
+            if(opcao < 1 || opcao > 4) {
+            erro_opcao();
+        }
+
+    switch(opcao){
+        
+        case 1:
+            //tamo criando tamo criando
+             break;
+
+        case 2:
+            //editar
+            menu_edit();
+            break;
+        
+        case 3:
+            //saboor listar tarefas
+            break;
+        
+        case 4:
+            //muy triste com sua saida
+            break;
+    }
+
+}
 }
 
 
@@ -20,6 +69,7 @@ void menu_edit(){ //menu para selecionar o que deseja editar
     int opcao = 1;
 
     while (opcao != 5){
+        limpar_tela();
         printf("==============================================\n");
         printf("\n\tSELECIONE O QUE DESEJA FAZER:\n\n");
         printf("==============================================\n");
@@ -32,15 +82,15 @@ void menu_edit(){ //menu para selecionar o que deseja editar
             if(opcao < 1 || opcao > 5) {
                 erro_opcao();
         
-            }else{
+            }else if(opcao != 5){
                 edit(opcao);
             }
-            
+               
     }
 }
 
 void edit(int numero){
-
+    limpar_tela();
     int x;
     printf("=============================================================");
     switch(numero){
@@ -56,11 +106,13 @@ void edit(int numero){
                 }else {
                     //variavel da prioridade = x;
                     printf("Prioridade alterada com sucesso.");
-                }
+                        esperar(1500);
+                }   
             break;
         case 2: 
             //if(variavel do item concluido == 0){variavel do item = 1;}else if(variavel do item concluido == 1) {variavel do item = 0;}
             printf("O item foi concluido com sucesso!");
+                esperar(1500);
             break;
         case 3:
             printf("Escolha um novo dia e horario:\n");
@@ -70,6 +122,7 @@ void edit(int numero){
             break;
         case 4: 
             printf("Item removido com sucesso");
+                esperar(1500);
             //variavel do nó = NULL;
             break;
 
